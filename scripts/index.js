24a1781e04d37e15dac0adcb673114f504550a65
Ravi Shukla <rshuklabbb@gmail.com>
2021-10-17 23:53:57 +0530

    Added reply and live updating of comment functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,39 @@
+function fetchData(){
+  return fetch('http://localhost:3000/posts')
+    .then( res => res.json())
+    .catch( err => console.log(err))
+}
+
+
+function fetchReply(id){
+  return fetch(`http://localhost:3000/replies?parentId=${id}`)
+    .then( res => res.json())
+    .catch( err => console.log(err));
+}
+
+ 
+function putComment(comment, id){
+  const author = "ravi";
+  const payload = {
+    parentId:id,
+    body:comment,
+    author:author,
+    points: 0,
+    timestamp: new Date().toGMTString()
+  };
+  config = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(payload)
+  }
+  return fetch("http://localhost:3000/replies", config)
+    .then( res => res.json() )
+    .catch( err => console.log( err ) )
+}
+
+
 function createBtns(){
   const reply = document.createElement("button");
   const giveAward = document.createElement("button");
@@ -32,7 +68,7 @@ function createMeta(post){
   return meta;
 }
 
-function renderPost(post) {
+async function renderPost(post) {
   const div = document.createElement("div");
   
   const meta = createMeta(post);
@@ -51,25 +87,42 @@ function renderPost(post) {
   replyBtn.textContent = "Comment"
   
   replyBtn.className = "reply-btn";
+  replyBtn.value = post.id;
   replyCont.className = "reply-box";
   replyCont.append( newReply, replyBtn );
 
   const arr = [meta, title, btns, replyCont];
 
-  if ( post.replies?.length > 0 ) {
-    const replies = document.createElement("div");
-    for (let reply of post.replies) {
-      const res = renderPost(reply);
-      replies.append(res);
-    }
-    replies.className = "reply";
+  const replies = await displayReplies(post.id);
+
+  if ( replies ) {
     arr.push(replies);
   }
+
   div.append(...arr);
   return div;
 }
 
-function handleClick() {
+async function displayReplies(id){
+  try {
+    const data = await fetchReply(id);
+    if ( data.length == 0 ){
+      return false;
+    }  
+    const replies = document.createElement("div");
+
+    for (let reply of data) {
+      const res = await renderPost(reply);
+      replies.append(res);
+    }
+    replies.className = "reply";
+    return replies;
+  } catch ( err ){
+    console.log(err);
+  }
+}
+
+function handleClick(event) {
   try {
     if ( event.target.textContent == "Reply" ){
       const tar = event.target.parentNode.parentNode.querySelector(".reply-box");
@@ -77,42 +130,53 @@ function handleClick() {
       
       // console.log(event.target.parentNode.parentNode)
     } else if ( event.target.className == "reply-btn" ){
-      // handle new event;
-
+      newComment(event)
     } else if ( event.target.className == "replies" ) {
-      console.log(event.target)
       const tar = event.target.querySelector(".reply");
       tar.style.display = tar.style.display == "block" ? "none" : "block";
     } else {
       const tar = event.target.parentNode.querySelector(".reply");
       tar.style.display = tar.style.display == "block" ? "none" : "block";
     }
+    event.stopImmediatePropagation();
   } catch ( err ){
     // error like replies doesn't exist
   } 
 }
 
-function fetchData(){
-  return fetch('http://localhost:3000/posts')
-    .then( res => res.json())
-    .catch( err => console.log(err))
-}
 
-async function handleData(){
+async function handleLoad(){
   try {
     const target = document.getElementById("container");
     const data = await fetchData();
-    const post = renderPost(data);
+    const post = await renderPost(data[0]);
     post.className = "post";
     target.append(post);
-
   } catch ( err ){
     console.log(err);
   }
+}
 
+async function newComment(){
+  try {
+    const parNode = event.target.parentNode;
+    const input = parNode.querySelector("input");
+    const parent = event.target.value;
+    const {id, parentId} = await putComment(input.value, parent);
+    const tar = parNode.parentNode;
+    const r = await displayReplies(parentId);
+    r.className = "reply"
+    r.style.display = "inherit";
+    if ( tar.querySelector(".reply") ){
+      tar.removeChild(tar.querySelector(".reply"))
+    }
+    tar.append( r );
+  } catch ( err ){
+    console.log(err);
+  }
 }
 
 window.addEventListener("load", () => {
-  handleData();
+  handleLoad();
   document.body.addEventListener("click", handleClick);
 });
